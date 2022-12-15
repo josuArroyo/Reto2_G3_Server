7beb3039c7d81c57cb6aa9da85599909b14ddddf
2dam <2dam@HZ375744.TartangaLH.eus>
2022-12-15 13:38:34 +0100

    entiti lugar

--- src/java/entities/Lugar.java
+++ src/java/entities/Lugar.java
@@ -5,44 +5,67 @@
  */
 package entities;
 
+import static entities.Evento_.lugar;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Pattern;
 
 /**
  *
  * @author 2dam
  */
 @Entity
+@Table(name = "lugar", schema = "Fuerza_g3")
+
 public class Lugar implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idLugar;
+
+    @Column
     private String nombre;
+
+    @Column
     private String descripcion;
+
+    @Column
     private String tipoLugar;
+
+    @Temporal(TemporalType.DATE)
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
     private Date tiempo;
 
     /**
      * @associates <{uml.Evento}>
      */
-    private List<Evento> listaEvento;
+    @OneToMany(mappedBy = "lugar")
+    private Set<Evento> listaEvento;
+
+    @ManyToOne
     private Admin admin;
 
-
- 
-
-    public void setListaEvento(List<Evento> listaEvento) {
+    public void setListaEvento(Set<Evento> listaEvento) {
         this.listaEvento = listaEvento;
     }
 
-    public List<Evento> getListaEvento() {
+    public Set<Evento> getListaEvento() {
         return listaEvento;
     }
 
@@ -57,8 +80,7 @@ public class Lugar implements Serializable {
     public void setIdLugar(Integer idLugar) {
         this.idLugar = idLugar;
     }
-    
-    
+
     public Integer getIdLugar() {
         return idLugar;
     }
@@ -119,5 +141,5 @@ public class Lugar implements Serializable {
     public String toString() {
         return "entities.Lugar[ id=" + idLugar + " ]";
     }
-    
+
 }
