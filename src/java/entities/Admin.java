541f8ff5ecbf61891b87d077233643a47aa3a4ab
Jessica <2dam@HZ375754.TartangaLH.eus>
2022-12-16 08:40:43 +0100

    Juntamos entities y commit de prueba

--- src/java/entities/Admin.java
+++ src/java/entities/Admin.java
@@ -28,10 +28,10 @@ public class Admin extends User {
     private static final long serialVersionUID = 1L;
     
     @GeneratedValue(strategy = GenerationType.AUTO)
-   
+    
     
     private String licencia;
-   
+    
 
     /**
      * @associates <{uml.Evento}>
